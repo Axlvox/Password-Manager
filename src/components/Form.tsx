e3693a1c20d269f52b51f071e32dc5b1d0e6c9fa
Axlvox <leandro.jlds@gmail.com>
2023-06-13 02:45:25 -0300

    Requisito 5

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,7 +7,6 @@ function Formulario() {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [validPassword, setValidPassword] = useState(false);
-
   const handleCadastrarSenha = () => {
     setShowButton(false);
     setShowForm(true);
@@ -65,10 +64,43 @@ function Formulario() {
             URL:
             <input type="text" placeholder="URL" />
           </label>
-          <button disabled={ !nameService || !login || !validPassword }>
+          <button
+            disabled={ !nameService || !login || !validPassword || password.length < 8
+    || password.length > 16 }
+          >
             Cadastrar
           </button>
           <button onClick={ handleCancelar }>Cancelar</button>
+          <span>
+            {password && password.length < 8 ? (
+              <div className="invalid-password-check">Possuir 8 ou mais caracteres</div>
+            ) : (
+              <div className="valid-password-check">Possuir 8 ou mais caracteres</div>
+            )}
+
+            {password.length <= 16 ? (
+              <div className="valid-password-check">Possuir até 16 caracteres</div>
+            ) : (
+              <div className="invalid-password-check">Possuir até 16 caracteres</div>
+            )}
+
+            {/[a-zA-Z]/.test(password) && /\d/.test(password) ? (
+              <div className="valid-password-check">Possuir letras e números</div>
+            ) : (
+              <div className="invalid-password-check">
+                Possuir letras e números
+              </div>
+            )}
+
+            {/[!@#$%^&*]/.test(password) ? (
+              <div className="valid-password-check">Possuir algum caractere especial</div>
+            ) : (
+              <div className="invalid-password-check">
+                Possuir algum caractere especial
+              </div>
+            )}
+          </span>
+
         </form>
       )}
     </div>
