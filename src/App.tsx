a3b16e87746729cfffbabbcdf4bfedf508a1e0a9
Axlvox <leandro.jlds@gmail.com>
2023-06-12 23:23:22 -0300

    Tentativa Requisito 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Formulario from './components/Form';
 function App() {
   return (
     <div>
-      Hello World
       <h1>Gerenciador de senhas</h1>
       <Formulario />
     </div>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,26 +1,49 @@
-const formulario = () => {
-  return (
-    <form>
-      <label>
-        Nome do serviço:
-        <input type="text" placeholder="Nome do Serviço" />
-      </label>
-      <label>
-        Login:
-        <input type="text" placeholder="Login" />
-      </label>
-      <label>
-        Senha:
-        <input type="password" placeholder="Senha" />
-      </label>
-      <label>
-        URL:
-        <input type="text" placeholder="URL" />
-      </label>
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
-    </form>
-  );
-};
+import React, { useState } from 'react';
 
-export default formulario;
+function Formulario() {
+  const [mostrarBotao, setMostrarBotao] = useState(true);
+  const [mostrarFormulario, setMostrarFormulario] = useState(false);
+
+  const handleCadastrarSenha = () => {
+    setMostrarBotao(false);
+    setMostrarFormulario(true);
+  };
+
+  const handleCancelar = () => {
+    setMostrarBotao(true);
+    setMostrarFormulario(false);
+  };
+
+  return (
+    <div>
+      {mostrarBotao && (
+        <button onClick={ handleCadastrarSenha }>Cadastrar nova senha</button>
+      )}
+
+      {mostrarFormulario ? (
+        <form>
+          <label>
+            Nome do serviço:
+            <input type="text" placeholder="Nome do Serviço" />
+          </label>
+          <label>
+            Login:
+            <input type="text" placeholder="Login" />
+          </label>
+          <label>
+            Senha:
+            <input type="password" placeholder="Senha" />
+          </label>
+          <label>
+            URL:
+            <input type="text" placeholder="URL" />
+          </label>
+          <button>Cadastrar</button>
+          <button onClick={ handleCancelar }>Cancelar</button>
+        </form>
+      ) : null}
+    </div>
+  );
+}
+
+export default Formulario;
