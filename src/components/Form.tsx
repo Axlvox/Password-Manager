3bbb6cf23fccfefaf00f0ea863365585665e836a
Axlvox <leandro.jlds@gmail.com>
2023-06-13 01:48:34 -0300

    Requisito 4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 
 function Formulario() {
-  const [ShowButton, setShowButton] = useState(true);
+  const [showButton, setShowButton] = useState(true);
   const [showForm, setShowForm] = useState(false);
+  const [nameService, setNameService] = useState('');
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [validPassword, setValidPassword] = useState(false);
 
   const handleCadastrarSenha = () => {
     setShowButton(false);
@@ -14,34 +18,59 @@ function Formulario() {
     setShowForm(false);
   };
 
+  const validatePassword = () => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
+    setValidPassword(passwordRegex.test(password));
+  };
+
   return (
     <div>
-      {ShowButton && (
+      {showButton && (
         <button onClick={ handleCadastrarSenha }>Cadastrar nova senha</button>
       )}
 
-      {showForm ? (
+      {showForm && (
         <form>
           <label>
             Nome do serviço:
-            <input type="text" placeholder="Nome do Serviço" />
+            <input
+              type="text"
+              placeholder="Nome do Serviço"
+              value={ nameService }
+              onChange={ (e) => setNameService(e.target.value) }
+            />
           </label>
           <label>
             Login:
-            <input type="text" placeholder="Login" />
+            <input
+              type="text"
+              placeholder="Login"
+              value={ login }
+              onChange={ (e) => setLogin(e.target.value) }
+            />
           </label>
           <label>
             Senha:
-            <input type="password" placeholder="Senha" />
+            <input
+              type="password"
+              placeholder="Senha"
+              value={ password }
+              onChange={ (e) => {
+                setPassword(e.target.value);
+                validatePassword();
+              } }
+            />
           </label>
           <label>
             URL:
             <input type="text" placeholder="URL" />
           </label>
-          <button>Cadastrar</button>
+          <button disabled={ !nameService || !login || !validPassword }>
+            Cadastrar
+          </button>
           <button onClick={ handleCancelar }>Cancelar</button>
         </form>
-      ) : null}
+      )}
     </div>
   );
 }
